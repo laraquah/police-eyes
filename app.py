40f38ea97c2a708cc62172d524f77897b28f9d15
laraquah <laraquahhh@gmail.com>
2024-09-26 13:09:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def play_live_camera():
     image = camera_input_live()
     uploaded_image = PIL.Image.open(image)
     uploaded_image_cv = cv2.cvtColor(np.array(uploaded_image), cv2.COLOR_RGB2BGR)
-    visualized_image = utils.predict_image(uploaded_image_cv, conf_threshold)
+    visualized_image = utils.predict_image(uploaded_image_cv)
     st.image(visualized_image, channels = "BGR")
 
 if source_radio == "WEBCAM":
