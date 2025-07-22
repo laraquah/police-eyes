c9aa2c21abfa9f6c93e50590aee03aa524a50c04
laraquah <laraquahhh@gmail.com>
2025-07-22 22:52:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -119,9 +119,7 @@ if confidence > 0.5:
         return frame
 
 # Start Webcam Stream
-if uploaded_file is None:
-    st.warning("Please upload a reference picture to start comparison.")
-elif reference_embedding is None:
-    st.warning("Face not detected in the reference image. Please upload a clearer photo.")
-else:
+if uploaded_file is not None and reference_embedding is not None:
     frame = camera_input_live(key="face_comparison", video_transformer_factory=FaceComparison)
+else:
+    st.warning("Please upload a reference image to start comparison.")
